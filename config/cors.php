bfbe500d0bde7eb5b585331cae6e288924d6b9b8
ztest95 <110767420+ztest95@users.noreply.github.com>
2025-07-02 14:25:31 +0800

    chore: cleanup - remove frontend related files - use env for frontend

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['http://localhost:5174'],
+    'allowed_origins_patterns' => [env('FRONTEND_URL'), 'http://localhost:5173'],
 
     'allowed_headers' => ['*'],
 
